215c413f7a26e98534834139045660e50afd2948
mas6y6 <125225333+mas6y6@users.noreply.github.com>
2025-11-13 21:43:36 -0600

    Fixed listeners

--- src/main/java/com/mas6y6/masworld/Commands/PersonalVault/PersonalVaultListener.java
+++ src/main/java/com/mas6y6/masworld/Commands/PersonalVault/PersonalVaultListener.java
@@ -3,8 +3,14 @@ package com.mas6y6.masworld.Commands.PersonalVault;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PersonalVaultListener implements Listener {
     public PersonalVault personalVault;
@@ -24,9 +30,33 @@ public class PersonalVaultListener implements Listener {
     }
 
     @EventHandler
-    public void onInventoryClick(InventoryClickEvent event) {
-        if (event.getInventory() instanceof VaultInventoryHolder inventory) {
-            inventory
+    public void onInventoryClose(InventoryCloseEvent event) {
+        if (event.getInventory().getHolder() instanceof VaultInventoryHolder holder) {
+
+            Map<Integer, ItemStack> items = new HashMap<>();
+            Inventory inv = event.getInventory();
+
+            for (int i = 0; i < inv.getSize(); i++) {
+                ItemStack item = inv.getItem(i);
+                if (item != null) items.put(i, item);
+            }
+
+            holder.getVault().saveItems(items);
         }
     }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent event) {
+        if (event.getInventory().getHolder() instanceof VaultInventoryHolder holder) {
+
+            if (event.getClickedInventory() != event.getView().getTopInventory()) {
+                event.setCancelled(true);
+            }
+
+            if (event.getClick().isShiftClick()) {
+                event.setCancelled(true);
+            }
+        }
+    }
+
 }
