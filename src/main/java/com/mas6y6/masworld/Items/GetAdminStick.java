29830934552ecc4658bce735bad61c0ffe01fa11
malachi196 <malachiaaronwilson@gmail.com>
2025-12-29 22:15:40 -0600

    changed admin stick range system from spigot to papermc

--- src/main/java/com/mas6y6/masworld/Items/GetAdminStick.java
+++ src/main/java/com/mas6y6/masworld/Items/GetAdminStick.java
@@ -6,6 +6,10 @@ import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
+//import org.bukkit.inventory.meta.components.AttackRangeComponent;
+//import io.papermc.paper.datacomponent.DataComponentType;
+import io.papermc.paper.datacomponent.DataComponentTypes;
+import io.papermc.paper.datacomponent.item.AttackRange;
 import java.lang.*;
 
 import com.mas6y6.masworld.Items.Attributes.Utils.SetWeaponDamage;
@@ -20,12 +24,21 @@ public class GetAdminStick {
         NamespacedKey key = new NamespacedKey("masworld", "adminstick")
         PersistentDataContainer container = adminStickIM.getPersistentDataContainer();
         container.set(key, PersistentDataType.BOOLEAN, true);
+        /*
         AttackRangeComponent adminStickIM_AR = adminStickIM.getAttackRange();
         if (adminStickIM_AR == null) {
             adminStickIM_AR = adminStickIM.getComponents().getAttackRange();
         }
         adminStickIM_AR.setValue(100);
         adminStickIM.setAttackRange(adminStickIM_AR);
+        */
+        AttackRange adminstickrange = AttackRange.attackRange()
+            .minReach(1.0f)
+            .maxReach(10.0f)
+            .minCreativeReach(0.5f)
+            .maxCreativeReach(100.0f)
+            .build();
+        _adminstick.setData(DataComponentTypes.ATTACK_RANGE, adminstickrange);
         adminStickIM.setDisplayName(adminStickName);
         adminStickIM.setUnbreakable(true);
         adminStickIM.addEnchant(Enchantment.CHANNELING, 1, true);
